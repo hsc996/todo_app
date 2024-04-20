aa8dd075733215b060074c2d65beceb0ba826b04
hsc996 <han.scaife@gmail.com>
2024-04-20 11:41:55 +1000

    implement remove to-do funciton

--- to_do_functions.py
+++ to_do_functions.py
@@ -7,8 +7,25 @@ def add_todo(file_name):
         writer = csv.writer(f) # This is a csv method, allowing us to write several roes of data at a time into the file
         writer.writerow([todo_name, "False"]) # Appending the item in list form
 
-def remove_todo():
-    print("Remove to-do")
+def remove_todo(file_name):
+    todo_name = input("Enter the to-do item that you would like to delete: ") # Laundry
+    # Create a new python list
+    todo_list = []
+    # Put all the previous items into the list except the one they want to delete
+    with open(file_name, "r") as f:
+        reader = csv.reader(f)
+        is_exist = False
+        for row in reader: # [Grocery shopping, False]
+            if (todo_name != row[0]): # != Laundry, which means Grocery shjopping --> True
+                todo_list.append(row) # Grocery will be appended to new list
+    # Write the entire list/csv file with this new list
+            else:
+                is_exist = True
+    if not is_exist:
+        print("Item name given does not exist.")
+    with open(file_name, "w") as f:
+        writer = csv.writer(f)
+        writer.writerows(todo_list) # writerows for multiple list items
 
 def mark_todo():
     print("Mark to-do")
